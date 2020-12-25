e8e18d648ddd7d5a3be63d2579e30e36c2da726c
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-12-25 12:57:41 +0600

    project init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "antd";
+import "./App.css";
+import Detail from "./components/detail/Detail";
+import Result from "./components/result/Result";
+import Search from "./components/search/Search";
+import AppContext from "./contexts/AppContext";
+
+const { Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="App">
+      <Content>
+        <AppContext>
+          <Search></Search>
+          <Result></Result>
+          <Detail></Detail>
+        </AppContext>
+      </Content>
+    </Layout>
   );
 }
 
