991a57091a86cfdb698bc1f5f9b7ed6aa8abf27b
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-12-25 13:13:18 +0600

    results diplayed

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { SearchContext } from "../../contexts/AppContext";
 
 function Detail() {
-  return <div>Detail</div>;
+  const { countryDetail } = useContext(SearchContext);
+  return (
+    <>
+      {countryDetail && (
+        <div>
+          <div>Country Name:{countryDetail.name} </div>
+          <div>Capital:{countryDetail.capital} </div>
+          <div>
+            Languages:
+            {countryDetail.languages.map((language, i) => (
+              <span key={i}>{language.name}</span>
+            ))}
+          </div>
+          {/* <div>Country Name: </div> */}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Detail;
--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -1,13 +1,18 @@
 import { Card } from "antd";
 import React, { useContext } from "react";
 import { SearchContext } from "../../contexts/AppContext";
+import "./Result.css";
 
 function Result() {
   const { result, showDetail } = useContext(SearchContext);
   return (
     <Card title="Card title" bordered={false} style={{ width: 300 }}>
       {result.map((country, i) => (
-        <div key={i} onClick={() => showDetail(country)}>
+        <div
+          key={i}
+          onClick={() => showDetail(country)}
+          className="result--list"
+        >
           {country.name}
         </div>
       ))}
